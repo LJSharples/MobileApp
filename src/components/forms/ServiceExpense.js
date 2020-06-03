8f13a033cfc0f0ca0759ce86163324e923aa9be3
LSharples <luke.sharples@powersolutionsuk.com>
2020-06-03 15:29:51 +0100

    Update SVG for services

--- src/components/forms/ServiceExpense.js
+++ src/components/forms/ServiceExpense.js
@@ -9,27 +9,17 @@ class ServiceExpense extends React.PureComponent{
         const data = [
             {
                 key: 1,
-                amount: 50,
+                amount: 100,
                 svg: { fill: '#600080' },
             },
             {
                 key: 2,
-                amount: 50,
-                svg: { fill: '#9900cc' }
-            },
-            {
-                key: 3,
-                amount: 40,
+                amount: 125,
                 svg: { fill: '#c61aff' }
             },
             {
-                key: 4,
-                amount: 95,
-                svg: { fill: '#d966ff' }
-            },
-            {
-                key: 5,
-                amount: 35,
+                key: 3,
+                amount: 45,
                 svg: { fill: '#ecb3ff' }
             }
         ]
