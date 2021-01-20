aa11d01b92df6845badd047f2355304a3bca71de
LSharples <luke.sharples@powersolutionsuk.com>
2021-01-20 09:07:41 +0000

    add back to login button on T&C's

--- src/components/screens/TermsConditionsScreen.js
+++ src/components/screens/TermsConditionsScreen.js
@@ -4,12 +4,15 @@ import {
     Text,
     ScrollView,
     RefreshControl,
-    TouchableOpacity
-  } from 'react-native'
-  import {
+    TouchableOpacity,
+    ImageBackground
+} from 'react-native'
+import {
     Item
-  } from 'native-base'
-  import { t } from 'react-native-tailwindcss';
+} from 'native-base'
+import { t } from 'react-native-tailwindcss';
+
+const background = require('../images/background.png')
 
 export default class TermsConditionsScreen extends React.Component {
     state = {
@@ -23,9 +26,13 @@ export default class TermsConditionsScreen extends React.Component {
         });
     }
 
+    handleRoute = async (destination) => {
+        await this.props.navigation.navigate(destination)
+    }
+
     render(){
         return (
-            <View style= {[ t.flex1, t.bgBlue200]}>
+            <ImageBackground source={background} style= {[ t.flex1]}>
                 <ScrollView
                     refreshControl={
                     <RefreshControl
@@ -37,15 +44,23 @@ export default class TermsConditionsScreen extends React.Component {
                     <Item style={[ t.mT5,t.alignCenter, t.borderTransparent]}>
                         <View style={[t.pX3, t.pY4, t.pt8, t.wFull]}>
                             <Item style={[t.pX2, t.pY2, t.pt4, t.itemsStart, t.justifyStart, t.borderTransparent]}>
-                                <Text style={[ t.text2xl, t.textBlue600]}>Terms & Conditions</Text>
+                                <Text style={[ t.text2xl, t.textWhite]}>Terms & Conditions</Text>
                             </Item>
                             <Item style={[t.pX2, t.pY2, t.pt4, t.itemsStart, t.justifyStart, t.borderTransparent]}>
-                                <Text style={[ t.textXl, t.textBlue600]}>Manage all your services in one place</Text>
+                                <Text style={[ t.textXl, t.textWhite]}>Manage all your services in one place</Text>
                             </Item>
                         </View>
                     </Item>
+                    <Item style={[t.itemsCenter, t.justifyCenter, t.borderTransparent, t.mT2]}>
+                        <TouchableOpacity 
+                            onPress={() => this.handleRoute('SignIn')}
+                            style={[t.p4, t.roundedLg, t.bgGray500, t.itemsCenter, t.w10_12]}
+                            >
+                            <Text style={[t.textWhite, t.textLg]}>Back to login</Text>
+                        </TouchableOpacity>
+                    </Item>
                 </ScrollView>
-            </View>
+            </ImageBackground>
         )
     }
 }
